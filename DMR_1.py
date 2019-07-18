dc6c39fe44e6115f1be56b37f250e68c5fdc1af9
sagesimhon <simhon@mit.edu>
2019-07-18 16:57:12 +0300

    bugs fixed in draw_z and log likelihood/derivate functions worked on

--- DMR_1.py
+++ DMR_1.py
@@ -10,7 +10,7 @@ import csv
 
 class DMR:
     """Topic Model with DMR"""
-    def __init__(self, phi, x, data, mu=0, sigma=1, epsilon=1e-4, max_iter=1e4):
+    def __init__(self, phi, x, data, clinical_data, mu=0, sigma=1, epsilon=1e-4, max_iter=1e4):
         # usually T=12, F=0, D=560, m=96
 
         ################################# Dimensions ###################################
@@ -22,12 +22,13 @@ class DMR:
         self.mu = mu  # mean of lambda's dist
         self.sigma = sigma  # variance of lambda's dist
 
-        self.phi = phi # (e from MMM) - signatures - T by m
+        self.phi = phi # (e from MMM) - signatures - T by m. probabilities
         temp = np.ones((self.D, self.F + 1))
         temp[:,1:] = x
-        self.x = temp # metadata - D by F with column of all 1s
+        self.x = temp # metadata - D by F+1 with column of all 1s
 
         self.data = data
+        self.clinical_data = clinical_data
 
         ################################################################################
 
@@ -39,6 +40,7 @@ class DMR:
             self.__calculate_alpha()
 
         self.z = None # np.empty((self.D, self.n_d))?
+        self.n_td = np.empty((self.D, self.T)) # D by T: each entry is how many times topic T drawn in sample D
         self.__draw_z()
 
         # For fitting to convergence:
@@ -55,7 +57,9 @@ class DMR:
         self.alpha = np.exp(np.dot(self.x, self.lam.T))
 
     def __draw_z(self):
-        """Draw z from distributions - vector of length n_d for each document, topic """
+        """Draw z from distributions - vector of length n_d for each document, topic
+           Record number of times topics t drawn for each document d in matrix self.n_td (D by T)
+        """
 
         # Form P
 
@@ -74,41 +78,59 @@ class DMR:
         _, m = self.phi.shape
         self.z = []
         parsed_data = [None]*self.D
-        for id, key in enumerate(self.data.keys()):
+        for id, key in enumerate(self.clinical_data): #enumerate(self.data.keys()): # For each sample, get categories and counts
             categories, counts = np.unique(self.data[key]['sequence'], return_counts=True)
             curr_z = np.zeros(len(self.data[key]['sequence']))
-            for j in range(len(categories)):
-                tmp_z = np.random.choice(np.arange(self.T), size=counts[j], p=P[id, categories[j]])
+            for j in range(len(categories)): # For each mutation category draw counts[j] topics for that particular document's counts array
+                tmp_z = np.random.choice(np.arange(self.T), size=counts[j], p=P[id, categories[j]]) # draw counts[j] samples for some mutation category j
                 curr_z[self.data[key]['sequence'] == categories[j]] = tmp_z
-            self.z.append(curr_z)
+            self.z.append(curr_z) # len of curr_z is n_d and number in each entry ranges from 0-11 (topics)
 
             parsed_data[id] = self.data[key]['sequence']
+            categories_t, counts_t = np.unique(curr_z, return_counts=True)
+            if id == 0: print('curr_z: ', [o for o in curr_z])
+            for t in range(self.T):
+                self.n_td[id][t] = counts_t[t]
 
-    def log_likelihood(self, parsed_data):
+    def log_likelihood(self, alph):
         """Log-likelihood P(z, lambda), using self.z and self.lam"""
         term1 = 1
         term2 = 1
         for d in range(self.D):
-            term1 *= gamma(self.alpha)/gamma(self.alpha+len(parsed_data[d]))
+            term1 *= gamma(np.sum(alph, axis=1))/gamma(np.sum(alph, axis=1)+np.sum(self.n_td, axis=1))
             for t in range(self.T):
-                term1 *= gamma(self.alpha + self.z[parsed_data[d]])
+                term1 *= gamma(alph[d][t] + self.n_td[d][t])/gamma(alph[d][t])
         for t in range(self.T):
             for f in range(self.F):
                 term2 *= 1/math.sqrt(2*math.pi*self.sigma**2)*math.exp(-self.lam[t][f]**2/(2*self.sigma**2))
 
         return term1*term2
-        # Questions: Is there a way to simplify these calculations? Also I don't think they are fully correct, can you take a look at them?
 
-    def d_log_likelihood(self, t, f, parsed_data):
-        """Derivative of log-likelihood P(z, lambda), using self.z and self.lam_t_k for topic t and feature f"""
-        res = np.sum(np.dot(self.x.T[f], self.alpha.T[t].T) * (digamma(np.sum(self.alpha, 1)) - digamma(np.sum(self.alpha+parsed_data, 1)) + digamma(self.alpha.T[t].T+parsed_data[t]) - digamma(self.alpha[t]))-self.lam[t][f]/self.sigma**2, 0)
+    def d_log_likelihood(self, alph):
+        """Derivative of log-likelihood P(z, lambda), using self.z and self.lam_t_k for topic t and feature f
+           NOTE: for now hard coded t and f and d as 0, 0, 0
+        """
+        t = 0
+        f = 0
+        d = 0
+
+        res = np.sum(np.dot(self.x.T[f], alph.T[t].T), axis=0) * (digamma(np.sum(alph, axis=1)) - digamma(np.sum(alph, axis=1)+np.sum(self.n_td, axis=1)) + digamma(alph[d][t]+self.n_td[d][t]) - digamma(alph[d][t]))-self.lam[t][f]/self.sigma**2
         return res
-        # todo fix n_t|d term and double check formula
 
     def optimize_lambda(self):
-        """Receives a lamda and finds new optimal lambda according to bfgs"""
+        """Receives a lamda and finds new optimal lambda according to bfgs
+           NOTE: currently optimizing alpha
+        """
+
+        def ll(alph):
+            return self.log_likelihood(alph)
+
+        def dll(alph):
+            return self.d_log_likelihood(alph)
+
+
         random_starting_point = np.random.rand(self.lam.shape[0], self.lam.shape[1])
-        newlam = optimize.fmin_l_bfgs_b(self.log_likelihood, random_starting_point, self.d_log_likelihood)[0]
+        newlam = optimize.fmin_l_bfgs_b(ll, random_starting_point, dll)[0]
         self.sigma = np.var(newlam, axis=1) #correct axis to sum over?
         self.mu = np.mean(newlam, axis=1) #correct axis to sum over?
         self.lam = newlam
@@ -116,6 +138,8 @@ class DMR:
 
     def fit(self):
         """Fit data by stochastic EM"""
+
+
         iters = 0
         while iters < self.max_iter:
             self.optimize_lambda()
@@ -136,22 +160,22 @@ def load_test_files(sequence_data_filename='simple_data/data_for_michael.json', 
         num_features = 4 #len(csv_reader[0])
         x = np.zeros((num_samples, num_features))
         row_idx = 0
+        clinical_data = []
         for row in csv_reader:
             if row_idx != 0:
                 #want array of cols 3 to 6 (features)
                 for feature_idx, val in enumerate(row[3:]):
                     x[row_idx-1][feature_idx] = val
+                clinical_data.append(row[0])
             row_idx += 1
         print(f'Processed {row_idx} lines.')
-    return phi, x, data
+    return phi, x, data, clinical_data
 
 
 def run_DMR(mu=0, sigma=1, epsilon=1e-4, max_iter=1e4):
-    phi, x, data = load_test_files()
-    model = DMR(phi, x, data, mu, sigma, epsilon, max_iter)
+    phi, x, data, clinical_data = load_test_files()
+    model = DMR(phi, x, data, clinical_data, mu, sigma, epsilon, max_iter)
     model.fit()
 
 if __name__ == "__main__" or __name__ == 'basic_model_MMM':
     run_DMR()
-
-#test commit
